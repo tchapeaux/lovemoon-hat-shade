8b9517c44ca8102bfe9151b11d0a0c85e99418e1
Thomas Chapeaux <chapeauxthomas@gmail.com>
2015-01-31 15:30:39 +0100

    Use `rsync` rather than `cp` to be osx-compatible

--- love-sublime-build.sh
+++ love-sublime-build.sh
@@ -9,22 +9,22 @@ mkdir -p gen_love
 if [ -d "src/lib" ] || [ -d "thirdparty/lib" ]; then
     mkdir -p gen_love/lib
     if [ -d "src/lib" ]; then
-        cp -ru ./src/lib ./gen_love
+        rsync -ru ./src/lib ./gen_love
     fi
 
     if [ -d "thirdparty/lib" ]; then
-        cp -ru thirdparty/lib ./gen_love
+        rsync -ru thirdparty/lib ./gen_love
     fi
 fi
 
 if [ -d "res" ]; then
     mkdir -p gen_love/res
-    cp -ru ./res ./gen_love
+    rsync -ru ./res ./gen_love
 fi
 
 if [ -d "data" ]; then
     mkdir -p gen_love/data
-    cp -ru ./data ./gen_love
+    rsync -ru ./data ./gen_love
 fi
 
 cd ./src
